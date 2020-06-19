335ff5589619e9d1eed1c9a1205fd9b54a4ee263
Jefferson <mirarajames29@gmail.com>
2020-06-19 06:01:15 -0700

    Add the changes made in the models and other files

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from . import db
-class User(db.Model,UserMixin):
+class User(db.Model):
     __tablename__="users"
     id=db.Column(db.Integer,primary_key=True)
     username=db.Column(db.String(30),nullable=False,unique=True)
